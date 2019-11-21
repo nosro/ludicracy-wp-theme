167f9e95c8d1956567926623a49dd1a80b4853ff
Orson Bradford <orsonbradford@gmail.com>
2019-11-21 14:33:12 -0500

    Posted date template tag

--- inc/template-tags.php
+++ inc/template-tags.php
@@ -11,7 +11,7 @@ if ( ! function_exists( 'orson_posted_on' ) ) :
 	/**
 	 * Prints HTML with meta information for the current post-date/time.
 	 */
-	function orson_posted_on() {
+	function orson_posted_on($format_label = 'Posted on %s', $linked = false) {
 		$time_string = '<time class="entry-date published updated" datetime="%1$s">%2$s</time>';
 		if ( get_the_time( 'U' ) !== get_the_modified_time( 'U' ) ) {
 			$time_string = '<time class="entry-date published" datetime="%1$s">%2$s</time><time class="updated" datetime="%3$s">%4$s</time>';
@@ -24,11 +24,19 @@ if ( ! function_exists( 'orson_posted_on' ) ) :
 			esc_html( get_the_modified_date() )
 		);
 
-		$posted_on = sprintf(
-			/* translators: %s: post date. */
-			esc_html_x( 'Posted on %s', 'post date', 'orson' ),
-			'<a href="' . esc_url( get_permalink() ) . '" rel="bookmark">' . $time_string . '</a>'
-		);
+		if ($linked) {
+			$posted_on = sprintf(
+				/* translators: %s: post date. */
+				esc_html_x( $format_label, 'post date', 'orson' ),
+				'<a href="' . esc_url( get_permalink() ) . '" rel="bookmark">' . $time_string . '</a>'
+			);
+		} else {
+			$posted_on = sprintf(
+				/* translators: %s: post date. */
+				esc_html_x( $format_label, 'post date', 'orson' ),
+				$time_string
+			);
+		}
 
 		echo '<span class="posted-on">' . $posted_on . '</span>'; // WPCS: XSS OK.
 
--- template-parts/content.php
+++ template-parts/content.php
@@ -17,10 +17,20 @@
 		else :
 			the_title( '<h2 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' );
 		endif;
+
+		if ( 'post' === get_post_type() ) :
 			?>
+			<div class="entry-meta">
+				<?php
+				orson_posted_on('%s');
+				?>
+			</div><!-- .entry-meta -->
+		<?php endif; ?>
 	</header><!-- .entry-header -->
 
-	<?php orson_post_thumbnail(); ?>
+	<figure class="entry-thumbnail">
+		<?php orson_post_thumbnail(); ?>
+	</figure>
 
 	<div class="entry-content">
 		<?php
